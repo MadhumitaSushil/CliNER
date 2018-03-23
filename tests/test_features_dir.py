89f7dd0edc32a7a93978eeaad658bccd309000a8
madhumita-git <madhumita154@gmail.com>
2018-03-23 15:54:32 +0100

    fixing imports

--- tests/test_features_dir.py
+++ tests/test_features_dir.py
@@ -4,22 +4,24 @@ if __name__ == '__main__':
     import doctest
     
     import os, sys
-    home = os.path.join( os.getenv('CLINER_DIR') , 'cliner' )
+
+    CLINER_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    home = os.path.join(CLINER_DIR , 'cliner' )
     if home not in sys.path: sys.path.append(home)
 
     #from features_dir import *
     
-    import features_dir.features
-    doctest.testmod(features_dir.features)
-    
-    import features_dir.read_config
-    doctest.testmod(features_dir.read_config)
-    
-    import features_dir.sentence_features
-    doctest.testmod(features_dir.sentence_features)
+    from code.feature_extraction import features
+    doctest.testmod(features)
 
-    import features_dir.utilities
-    doctest.testmod(features_dir.utilities)
+    from code.feature_extraction import read_config
+    doctest.testmod(read_config)
 
-    import features_dir.word_features
-    doctest.testmod(features_dir.word_features)
+    from code.feature_extraction import sentence_features
+    doctest.testmod(sentence_features)
+
+    from code.feature_extraction import utils
+    doctest.testmod(utils)
+
+    from code.feature_extraction import word_features
+    doctest.testmod(word_features)
